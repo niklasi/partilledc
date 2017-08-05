7e676b31623cccf34c16a38a489bf5bdad82b3c1
Niklas Ingholt <niklas@ingholt.com>
2017-08-05 10:51:34 +0200

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -43,7 +43,6 @@ const initAuth = (dispatch) => {
     firebaseAuth.onAuthStateChanged(
       user => {
         dispatch(currentUser(user))
-        // unsub()
         resolve()
       },
       error => reject(error)
