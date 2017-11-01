03a5b97abb790954e5d71c3a7d45045319cf7967
Niklas Ingholt <niklas@ingholt.com>
2017-11-01 20:17:48 +0100

    Enable pin code

--- src/components/Matches/Match.js
+++ src/components/Matches/Match.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types' 
+import PropTypes from 'prop-types'
 import { Card, CardHeader } from 'material-ui/Card'
+import TextField from 'material-ui/TextField'
 import Avatar from 'material-ui/Avatar'
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
@@ -10,17 +11,22 @@ import RegisterResult from './RegisterResult'
 import { matchPoints } from '../../lib/partilledc-score'
 
 const colMapper = ({ text, result = [] }) => <TableRowColumn key={text} style={{whiteSpace: 'normal'}}>
-                                               {result.filter(r => r.home !== 0 || r.away !== 0).map(r => r.home + '-' + r.away).join(', ')}
-                                             </TableRowColumn>
+  {result.filter(r => r.home !== 0 || r.away !== 0).map(r => r.home + '-' + r.away).join(', ')}
+</TableRowColumn>
 
 class Match extends Component {
-
   constructor (props) {
     super(props)
     this.handleOpen = this.handleOpen.bind(this)
     this.handleClose = this.handleClose.bind(this)
     this.changeResult = this.changeResult.bind(this)
-    this.state = {open: false, match: {text: '', result: [{home: 0, away: 0}]}}
+    this.isPinEnabled = this.isPinEnabled.bind(this)
+    this.requirePin = this.requirePin.bind(this)
+    this.state = {open: false, requirePin: this.isPinEnabled(), wrongPin: true, match: {text: '', result: [{home: 0, away: 0}]}}
+  }
+
+  isPinEnabled () {
+    return this.props.user.uid === 'c7RECUVjoIM1iHB7jvldxScB0C62'
   }
 
   handleOpen (match) {
@@ -29,6 +35,16 @@ class Match extends Component {
 
   handleClose () {
     this.setState({open: false})
+    this.setState({requirePin: false})
+    this.requirePin()
+  }
+
+  requirePin () {
+    if (!this.isPinEnabled()) return
+    clearTimeout(this.timerId)
+    this.timerId = setTimeout(() => {
+      this.setState({requirePin: true, wrongPin: true})
+    }, 1000 * 60 * 2)
   }
 
   changeResult (matchResult) {
@@ -44,7 +60,7 @@ class Match extends Component {
     const match = this.props.match
 
     const actions = [
-      <FlatButton label='Stäng' primary onTouchTap={this.handleClose} />
+      <FlatButton label='Stäng' primary onTouchTap={this.handleClose} disabled={this.state.requirePin && this.state.wrongPin} />
     ]
 
     const {palette} = getMuiTheme()
@@ -56,40 +72,48 @@ class Match extends Component {
         }
       }
       return <TableHeaderColumn key={item.text} style={{paddingLeft: '5px'}}>
-               <FlatButton
-                 label={item.text}
-                 primary
-                 labelStyle={{textTransform: 'none'}}
-                 onTouchTap={open} />
-             </TableHeaderColumn>
+        <FlatButton
+          label={item.text}
+          primary
+          labelStyle={{textTransform: 'none'}}
+          onTouchTap={open} />
+      </TableHeaderColumn>
+    }
+
+    const view = (requirePin) => {
+      if (requirePin) {
+        return <TextField hintText='Ange pin' pattern='[0-9]*' inputMode='numeric' onChange={(e, newValue) => this.setState({wrongPin: newValue !== '1958'})} />
+      }
+
+      return <RegisterResult onChangeResult={this.changeResult} match={this.state.match} />
     }
 
     const formatMatchPoints = (score) => `${score.home.points}-${score.away.points}`
     return <Card>
-             <CardHeader style={{overflow: 'hidden'}} avatar={<Avatar size={35} backgroundColor={palette.accent1Color}>
-                                   {formatMatchPoints(matchPoints(match.matches.map(m => m.result)))}
-                                 </Avatar>} title={`${match.homeTeam.teamName} - ${match.awayTeam.teamName}`} subtitle={'Bana ' + match.lane + ' ' + match.date + ' kl ' + match.time} />
-             <Table selectable={false} multiselectable={false}>
-               <TableHeader displaySelectAll={false} adjustForCheckbox={false} enableSelectAll={false}>
-                 <TableRow>
-                   {match.matches.map(header)}
-                 </TableRow>
-               </TableHeader>
-               <TableBody displayRowCheckbox={false}>
-                 <TableRow>
-                   {match.matches.map(colMapper)}
-                 </TableRow>
-               </TableBody>
-             </Table>
-             <Dialog
-               title='Resultat'
-               actions={actions}
-               contentStyle={{width: '100%', maxWidth: 'none'}}
-               modal
-               open={this.state.open}>
-               <RegisterResult onChangeResult={this.changeResult} match={this.state.match} />
-             </Dialog>
-           </Card>
+      <CardHeader style={{overflow: 'hidden'}} avatar={<Avatar size={35} backgroundColor={palette.accent1Color}>
+        {formatMatchPoints(matchPoints(match.matches.map(m => m.result)))}
+      </Avatar>} title={`${match.homeTeam.teamName} - ${match.awayTeam.teamName}`} subtitle={'Bana ' + match.lane + ' ' + match.date + ' kl ' + match.time} />
+      <Table selectable={false} multiselectable={false}>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false} enableSelectAll={false}>
+          <TableRow>
+            {match.matches.map(header)}
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          <TableRow>
+            {match.matches.map(colMapper)}
+          </TableRow>
+        </TableBody>
+      </Table>
+      <Dialog
+        title={this.state.requirePin ? 'Ange pin' : 'Resultat'}
+        actions={actions}
+        contentStyle={{width: '100%', maxWidth: 'none'}}
+        modal
+        open={this.state.open}>
+        { view(this.state.requirePin) }
+      </Dialog>
+    </Card>
   }
 }
 
