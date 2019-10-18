9735234338f5f7561d2af1ce06983fd1731d9d23
Niklas Ingholt <niklas@ingholt.com>
2019-10-19 00:46:27 +0200

    Enable cors for resetSeries

--- functions/index.js
+++ functions/index.js
@@ -6,136 +6,143 @@ const r2 = require('r2')
 admin.initializeApp(functions.config().firebase)
 
 exports.resetSeries = functions.https.onRequest((request, response) => {
-  const seriesId = request.query.seriesId
-  const uid = request.query.uid
+  cors(request, response, () => {
+    const seriesId = request.query.seriesId
+    const slug = request.query.slug
+    const uid = request.query.uid
 
-  if (!seriesId) {
-    return response.status(400).send('Missing seriesId')
-  }
+    if (!seriesId) {
+      return response.status(400).send('Missing seriesId')
+    }
 
-  if (!uid) {
-    return response.status(400).send('Missing uid')
-  }
+    if (!slug) {
+      return response.status(400).send('Missing slug')
+    }
 
-  const teamsRef = '/teamsNext'
-  const matchesRef = '/matchesNext'
-  const db = admin.database()
+    if (!uid) {
+      return response.status(400).send('Missing uid')
+    }
 
-  function cleanSeries () {
-    const teamsPromise = new Promise(function (resolve, reject) {
-      db.ref(teamsRef)
-        .orderByChild('series')
-        .equalTo(seriesId)
-        .once('value', (snapshot) => {
-          snapshot.forEach(team => {
+    const teamsRef = '/teamsNext'
+    const matchesRef = '/matchesNext'
+    const db = admin.database()
+
+    function cleanSeries () {
+      const teamsPromise = new Promise(function (resolve, reject) {
+        db.ref(teamsRef)
+          .orderByChild('series')
+          .equalTo(seriesId)
+          .once('value', (snapshot) => {
+            snapshot.forEach(team => {
             // const {teamName} = team.val()
             // console.log(teamName, team.key)
-            db.ref(teamsRef + '/' + team.key).remove()
+              db.ref(teamsRef + '/' + team.key).remove()
+            })
+            resolve()
           })
-          resolve()
-        })
-    })
+      })
 
-    const matchesPromise = new Promise(function (resolve) {
-      db.ref(matchesRef)
-        .orderByChild('series')
-        .equalTo(seriesId)
-        .once('value', (snapshot) => {
-          snapshot.forEach(match => {
+      const matchesPromise = new Promise(function (resolve) {
+        db.ref(matchesRef)
+          .orderByChild('series')
+          .equalTo(seriesId)
+          .once('value', (snapshot) => {
+            snapshot.forEach(match => {
             // const {homeTeam, awayTeam, date} = match.val()
             // console.log(homeTeam.teamName + ' - ' + awayTeam.teamName + ', ' + date, match.key)
-            db.ref(matchesRef + '/' + match.key).remove()
-          })
-          resolve()
-        })
-    })
-
-    return Promise.all([teamsPromise, matchesPromise])
-  }
-
-  function migrateTeams (teams) {
-    return new Promise(function (resolve) {
-      teams
-        .map((team) => {
-          return {
-            teamName: team.team_name,
-            series: seriesId,
-            teamRanking: team.team_ranking,
-            contact: team.contact,
-            phone: team.phone,
-            email: team.email
-          }
-        })
-        .forEach((team) => {
-          db.ref(teamsRef).push(team)
-          // console.log(team.teamName)
-        })
-      resolve()
-    })
-  }
-
-  function migrateMatches (matches) {
-    return new Promise(function (resolve) {
-      db.ref(teamsRef)
-        .orderByChild('series')
-        .equalTo(seriesId)
-        .once('value', (snapshot) => {
-          let teams = []
-          snapshot.forEach(team => {
-            const {teamRanking, teamName} = team.val()
-            teams.push({id: team.key, teamRanking, teamName})
-          })
-
-          matches
-            .forEach(match => {
-              let homeTeam = teams
-                .find(team => team.teamRanking === match.home_team)
-              let awayTeam = teams
-                .find(team => team.teamRanking === match.away_team)
-
-              homeTeam.matchp = 0
-              awayTeam.matchp = 0
-
-              const {lanes, time, date} = match
-
-              const matches = lanes.indexOf('+') === -1
-                ? [
-                  {text: 'Match', result: [{home: 0, away: 0}]}
-                ]
-                : [
-                  { text: 'Dubbel', result: [ { home: 0, away: 0 } ] },
-                  { text: '1:a singel', result: [ { home: 0, away: 0 } ] },
-                  { text: '2:a singel', result: [ { home: 0, away: 0 } ] }
-                ]
-
-              const migratedMatch = {
-                homeTeam,
-                awayTeam,
-                date,
-                time,
-                lane: lanes,
-                matches,
-                series: seriesId}
-
-              db.ref(matchesRef).push(migratedMatch)
-              // console.log(migratedMatch.homeTeam.teamName + ' - ' + migratedMatch.awayTeam.teamName + ', ' + date + ' kl ' + time)
+              db.ref(matchesRef + '/' + match.key).remove()
             })
-          resolve()
-        })
-    })
-  }
+            resolve()
+          })
+      })
 
-  db.ref('/users/' + uid).once('value', async function (user) {
-    const admin = user.val().admin || false
-    if (!admin) {
-      response.status(401).send('Only admin can reset series')
+      return Promise.all([teamsPromise, matchesPromise])
     }
-    const data = await r2.get('https://partilletennis-scraper-js7ld72o6a-uc.a.run.app/scraper/lagserien/1').json
-    await cleanSeries()
-    await migrateTeams(data.teams)
-    await migrateMatches(data.matches)
 
-    return response.status(200).send('Serien uppdaterad')
+    function migrateTeams (teams) {
+      return new Promise(function (resolve) {
+        teams
+          .map((team) => {
+            return {
+              teamName: team.team_name,
+              series: seriesId,
+              teamRanking: team.team_ranking,
+              contact: team.contact,
+              phone: team.phone,
+              email: team.email
+            }
+          })
+          .forEach((team) => {
+            db.ref(teamsRef).push(team)
+          // console.log(team.teamName)
+          })
+        resolve()
+      })
+    }
+
+    function migrateMatches (matches) {
+      return new Promise(function (resolve) {
+        db.ref(teamsRef)
+          .orderByChild('series')
+          .equalTo(seriesId)
+          .once('value', (snapshot) => {
+            let teams = []
+            snapshot.forEach(team => {
+              const {teamRanking, teamName} = team.val()
+              teams.push({id: team.key, teamRanking, teamName})
+            })
+
+            matches
+              .forEach(match => {
+                let homeTeam = teams
+                  .find(team => team.teamRanking === match.home_team)
+                let awayTeam = teams
+                  .find(team => team.teamRanking === match.away_team)
+
+                homeTeam.matchp = 0
+                awayTeam.matchp = 0
+
+                const {lanes, time, date} = match
+
+                const matches = lanes.indexOf('+') === -1
+                  ? [
+                    {text: 'Match', result: [{home: 0, away: 0}]}
+                  ]
+                  : [
+                    { text: 'Dubbel', result: [ { home: 0, away: 0 } ] },
+                    { text: '1:a singel', result: [ { home: 0, away: 0 } ] },
+                    { text: '2:a singel', result: [ { home: 0, away: 0 } ] }
+                  ]
+
+                const migratedMatch = {
+                  homeTeam,
+                  awayTeam,
+                  date,
+                  time,
+                  lane: lanes,
+                  matches,
+                  series: seriesId}
+
+                db.ref(matchesRef).push(migratedMatch)
+              // console.log(migratedMatch.homeTeam.teamName + ' - ' + migratedMatch.awayTeam.teamName + ', ' + date + ' kl ' + time)
+              })
+            resolve()
+          })
+      })
+    }
+
+    db.ref('/users/' + uid).once('value', async function (user) {
+      const admin = user.val().admin || false
+      if (!admin) {
+        response.status(401).send('Only admin can reset series')
+      }
+      const data = await r2.get('https://partilletennis-scraper-js7ld72o6a-uc.a.run.app/scraper/' + slug).json
+      await cleanSeries()
+      await migrateTeams(data.teams)
+      await migrateMatches(data.matches)
+
+      return response.status(200).send('Serien nollställd')
+    })
   })
 })
 
