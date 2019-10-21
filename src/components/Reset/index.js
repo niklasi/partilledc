1ee6fcdf905687a4fc1a3771f00b200dce4ee333
Niklas Ingholt <niklas@ingholt.com>
2019-10-21 16:44:11 +0200

    Confirm reset series

--- src/components/Reset/index.js
+++ src/components/Reset/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table'
+import Dialog from 'material-ui/Dialog'
 import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import { connect } from 'react-redux'
 import { loadScrapedSeries, resetSeries } from '../../actions'
 import allSeries from '../../series.json'
 
 class Reset extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {open: false}
+    this.toggleDialog = this.toggleDialog.bind(this)
+  }
+
   componentDidMount () {
     this.props.loadScrapedSeries(this.props.slug)
   }
@@ -14,6 +22,12 @@ class Reset extends React.Component {
     if (nextProps.slug !== this.props.slug) {
       this.props.loadScrapedSeries(nextProps.slug)
     }
+  }
+
+  toggleDialog () {
+    const open = this.state.open
+
+    this.setState({open: !open})
   }
 
   render () {
@@ -25,6 +39,22 @@ class Reset extends React.Component {
     const displayContact = companySeries ? undefined : 'none'
     const teams = this.props.scrapedData.teams || []
     const matches = this.props.scrapedData.matches || []
+    const actions = [
+      <FlatButton
+        label='Avbryt'
+        secondary
+        onClick={this.toggleDialog}
+      />,
+      <FlatButton
+        label='Nollställ'
+        primary
+        onClick={() => {
+          this.props.resetSeries(series, slug, uid)
+          this.toggleDialog()
+        }
+        }
+      />
+    ]
 
     return (<div>
       <Table selectable={false}>
@@ -102,7 +132,18 @@ class Reset extends React.Component {
           </TableRow>)}
         </TableBody>
       </Table>
-      <RaisedButton label='Nollställ serien' secondary fullWidth onClick={() => this.props.resetSeries(series, slug, uid)} />
+      <RaisedButton label='Nollställ serien' secondary fullWidth onClick={this.toggleDialog} />
+      <Dialog
+        title='Nollställ serie'
+        actions={actions}
+        modal={false}
+        open={this.state.open}
+        onRequestClose={this.toggleDialog}
+      >
+          Du är på väg att nollställa serien. Detta innebär att nuvarande
+        resultat i serien kommer att tas bort och lag och spelschema kommer att ersättas med
+        det som finns på denna sidan.
+      </Dialog>
     </div>)
   }
 }
