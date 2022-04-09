51f6d740fddb75b48f58590e54017af20518de61
Niklas Ingholt <niklas@ingholt.com>
2022-04-10 01:59:12 +0200

    refactor: self host roboto font

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import { Provider } from 'react-redux'
 import configureStore from './store'
 import { firebaseAuth } from './firebase'
 import { currentUser } from './actions'
+import '@fontsource/roboto'
 
 injectTapEventPlugin()
 require('./assets/index.css')
