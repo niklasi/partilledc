2ddd22eb9a4f627af9ae380eb1c77304c67dd910
Niklas Ingholt <niklas@ingholt.com>
2022-04-10 01:16:59 +0200

    chore: extend tailwinds default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        primary: 'rgb(0,188,212)',
+        secondary: 'rgb(255,64,129)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
