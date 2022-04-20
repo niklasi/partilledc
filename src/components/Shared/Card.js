63d85e0dd13697bb923fdb389329addb2ce5c2ae
Niklas Ingholt <niklas@ingholt.com>
2022-04-21 01:31:58 +0200

    feat: create custom card component

---
+++ src/components/Shared/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div className='flex flex-row space-x-3 px-3 py-3 shadow border border-solid border-gray-200'>
+
+      <div className='h-9 w-9 flex justify-center items-center bg-secondary border border-secondary rounded-full'>
+        <p className='text-white'>{props.avatar}</p>
+      </div>
+      <div className='flex flex-col basis-11/12'>
+        <p className='text-base'>{props.title}</p>
+        <p className='text-sm text-gray-600'>{props.subtitle}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
