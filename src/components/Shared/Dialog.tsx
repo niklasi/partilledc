aab77de3b41996c0b8d4bec740cc59b86c84ea07
Niklas Ingholt <niklas@ingholt.com>
2022-07-23 00:41:32 +0200

    refactor: use headlessui for Dialog

--- src/components/Shared/Dialog.tsx
+++ src/components/Shared/Dialog.tsx
@@ -1,23 +1,23 @@
+import {Dialog} from '@headlessui/react'
+
 type DialogProps = {
     open: boolean
     title?: string
     children: React.ReactNode
     actions: JSX.Element[]
 }
-const Dialog = (props: DialogProps) => {
+export default function InternalDialog(props: DialogProps) {
     return (
-        <div
-            className={`bg-black/40 flex flex-col justify-center z-40 absolute top-0 left-0 w-screen h-screen ${
-                props.open ? undefined : 'hidden'
-            }`}
-        >
-            <div className="bg-white z-50 flex flex-col shadow-xl w-full px-4 py-4">
-                <h3 className="text-2xl">{props.title}</h3>
-                {props.children}
-                <div className="flex flex-row justify-end">{props.actions}</div>
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        <Dialog open={props.open} onClose={() => {}} className="relative z-50">
+            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+            <div className="fixed inset-0 flex items-center justify-center">
+                <Dialog.Panel className="bg-white w-full px-4 py-4">
+                    <Dialog.Title className="text-2xl">{props.title}</Dialog.Title>
+                    {props.children}
+                    <div className="flex flex-row justify-end">{props.actions}</div>
+                </Dialog.Panel>
             </div>
-        </div>
+        </Dialog>
     )
 }
-
-export default Dialog
