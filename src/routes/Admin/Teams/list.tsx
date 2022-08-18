47d1e10adb06668d128592e6074440d4bad03d22
Niklas Ingholt <niklas@ingholt.com>
2022-08-18 17:30:15 +0200

    feat: set team ranking

--- src/routes/Admin/Teams/list.tsx
+++ src/routes/Admin/Teams/list.tsx
@@ -1,6 +1,6 @@
-import {Link, useLoaderData} from 'react-router-dom'
+import {Link, useLoaderData, useParams, useNavigate} from 'react-router-dom'
 import {useCurrentSerie} from '../../../hooks/useCurrentSerie'
-import {getTeamsBySeries} from '../../../lib/api'
+import {getTeamsBySeries, saveTeam} from '../../../lib/api'
 
 export async function loader({params}) {
     return getTeamsBySeries(params.series)
@@ -9,8 +9,23 @@ export async function loader({params}) {
 export default function List() {
     const teams = useLoaderData() as Awaited<ReturnType<typeof loader>>
     const serie = useCurrentSerie()
+    const {series} = useParams()
+    const navigate = useNavigate()
 
     const isCompanySeries = serie.type === 'CompanySeries'
+
+    async function moveTeam(direction: 'up' | 'down', index: number) {
+        const team = teams[index]
+        const switchWithIndex = direction === 'up' ? index - 1 : index + 1
+        const switchWithTeam = teams[switchWithIndex]
+
+        team.teamRanking = switchWithIndex + 1
+        switchWithTeam.teamRanking = index + 1
+
+        await Promise.all([saveTeam(team), saveTeam(switchWithTeam)])
+
+        navigate(`/admin/${series}/teams`)
+    }
 
     return (
         <table className="md:table-fixed border border-collapse w-full">
@@ -21,10 +36,11 @@ export default function List() {
                     <th className="font-normal text-xs">Telefon</th>
                     <th className="font-normal text-xs">Email</th>
                     <th className="font-normal text-xs"></th>
+                    <th className="font-normal text-xs w-16"></th>
                 </tr>
             </thead>
             <tbody className="text-sm text-left">
-                {teams.map((team) => {
+                {teams.map((team, index) => {
                     return (
                         <tr key={team.id} className="divide-y divide-solid border h-12">
                             <td className="px-3">{team.teamName}</td>
@@ -35,6 +51,18 @@ export default function List() {
                                 <Link className="underline" to={team.id}>
                                     Editera
                                 </Link>
+                            </td>
+                            <td>
+                                <button onClick={() => moveTeam('down', index)} disabled={index === teams.length - 1} className='disabled:text-gray-300'>
+                                    <span className={'material-icons-outlined h-5 w-5'}>
+                                        expand_more
+                                    </span>
+                                </button>
+                                <button onClick={() => moveTeam('up', index)} disabled={index === 0} className='disabled:text-gray-300'>
+                                    <span className={'material-icons-outlined h-5 w-5'}>
+                                        expand_less
+                                    </span>
+                                </button>
                             </td>
                         </tr>
                     )
